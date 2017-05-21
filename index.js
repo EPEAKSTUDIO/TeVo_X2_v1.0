5426d1893d749c8db55c933243d1a6bc817ca6a2
Xyala <Xyala@Xyala.WORKGROUP>
2017-05-21 15:39:32 +0800

    test node v4

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 // testing for the thermocouple reading on TeVo X2 v1.0
 
-var max31855pi = require('max31855pi')
-var listener = max31855pi({ cs: 22, so: 9, sck: 11 })  // Needs to be updated to the right values for TeVo
 
-listener.on('temperature', function (temp) {
+// For reference, TeVo has 2 MAX31855 chips.
+// #1
+// CLK = GPIO11
+// MSIO = GPIO09
+// CE = GPIO08
+//#2
+// CLK = GPIO11
+// MSIO = GPIO09
+// CE = GPIO07
+
+var max31855pi = require('max31855pi')
+var temp1 = max31855pi({ cs: 8, so: 9, sck: 11 })  //
+
+temp1.on('temperature', function (temp) {
   console.log('Current temp =', temp, 'C')
 })
 
-listener.start()
+temp1.start()
