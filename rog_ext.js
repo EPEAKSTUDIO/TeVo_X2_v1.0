3114fa28e19c6d6642ed1ae700b1afd9fd0e9c17
Xyala <Xyala@Xyala.local>
2017-05-23 20:05:06 +0800

    add conversion functions for ROG_EXT in separate file

---
+++ rog_ext.js
@@ -0,0 +1,31 @@
+/**
+ * Node.js module for adapating the readings over I2c from the ROG_EXT on Raspberry Pi.
+ *
+ * Code written by Gergely Imreh - Huge thanks!
+ */
+
+/* Display in HEX and zero pad */
+function pad(n, width, z) {
+  z = z || '0';
+  n = n.toString(16) + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
+/* Change endiannes / swap two bytes of a word */
+function swap16(val) {
+  return ((val & 0xFF) << 8) | ((val >> 8) & 0xFF);
+}
+
+function toClk(rawValue) {
+  return swap16(rawValue) * 0.1;
+}
+
+/* Volts conversion */
+function toVolts(rawValue) {
+  return swap16(rawValue) * 0.005;
+}
+
+/* Fan RPM conversion */
+function toRPM(rawValue) {
+  return swap16(rawValue);
+}
